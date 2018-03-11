e302b114629f4edd68385fdcb5783d068b9d23dd
Amidou Kante <kamdiou@gmail.com>
2018-03-10 22:23:07 -0500

    created a responsive container

---
+++ client/src/components/Linechart.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import stats from '../data/top-100-players-pergame-2016-17';
+// import ReactHighCharts from 'react-highcharts';
+import {LineChart, Line, XAxis, Tooltip, CartesianGrid, YAxis, Legend, ResponsiveContainer } from 'recharts';
+
+const data = [
+    {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+    {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+    {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+    {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+    {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+    {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+    {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+];
+
+class Linechart extends Component {
+    constructor(){
+        super();
+        this.state - {
+
+        }
+    }
+    render(){
+        return(
+            <ResponsiveContainer>
+                <LineChart width={600} height={300} data={stats.splice(0,10)} margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+                    <XAxis dataKey="player"/>
+                    <YAxis/>
+                    <CartesianGrid strokeDasharray="3 3"/>
+                    <Tooltip/>
+                    <Legend />
+                    <Line type="monotone" dataKey="GP" stroke="#8884d8" activeDot={{r: 8}}/>
+                    <Line type="monotone" dataKey="PTS" stroke="#82ca9d" />
+                </LineChart>
+            </ResponsiveContainer>
+        )
+    }
+}
+
+export default Linechart;
