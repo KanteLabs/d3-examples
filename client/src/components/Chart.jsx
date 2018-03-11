12b4a7eb2fcfb2f368320084dc8a58ad5816ebb1
Amidou Kante <kamdiou@gmail.com>
2018-03-10 20:05:18 -0500

    components

---
+++ client/src/components/Chart.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Chart extends Component {
+
+    render(){
+        return(
+            <h1>cat</h1>
+        )
+    }
+}
+
+export default Chart;
