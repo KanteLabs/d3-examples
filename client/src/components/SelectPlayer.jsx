c4c84e669bbc5a59da8227bd2b0b65254d5b4f54
Amidou Kante <kamdiou@gmail.com>
2018-03-11 21:01:25 -0400

    sorted names in alphabetical order

--- client/src/components/SelectPlayer.jsx
+++ client/src/components/SelectPlayer.jsx
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import playerNames from '../data/player-names';
 
 class SelectPlayer extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            playerList: {}
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            playerList: playerNames
+        })
+    }
 
     renderNames = () => {
         return(
-            <select className="player-list" name="player-name">
+            <select className="player-list" name="player-name" onChange={(e)=>console.log(e.target.value)}>
                 {Object.values(playerNames).map((item, i)=>{
                     return(
                         <option value={item.player} key={i}>{item.player}</option>
