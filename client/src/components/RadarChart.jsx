164d3500c2aee88cbbba9f606179cf8e4ca975c3
Amidou Kante <kamdiou@gmail.com>
2018-03-14 22:47:44 -0400

    can filter by season type or player type

--- client/src/components/RadarChart.jsx
+++ client/src/components/RadarChart.jsx
@@ -18,13 +18,21 @@ class  TwoLevelPieChart extends Component {
             currentPlayer: "Stephen Curry",
             season: 'regular',
             playerNames: RegularNames,
-            data: [
-                {category: 'Steals', player_one: 1.2, player_two: RegularNames["Stephen Curry"].STL, cap: 30},
-                {category: 'Rebounds', player_one: 8.6, player_two: RegularNames["Stephen Curry"].REB, cap: 30},
-                {category: 'Assists', player_one:8.7, player_two: RegularNames["Stephen Curry"].AST, cap: 30},
-                {category: 'Free Throws', player_one: 4.8, player_two: RegularNames["Stephen Curry"].FTM, cap: 30},
-                {category: 'Turnovers', player_one: 4.1, player_two: RegularNames["Stephen Curry"].TOV, cap: 30},
-                {category: 'Field Goals', player_one: 9.9, player_two: RegularNames["Stephen Curry"].FGM, cap: 30},
+            regularData: [
+                {category: 'Steals', player_one: 1.2, player_two: RegularStats["Stephen Curry"].STL, cap: 30},
+                {category: 'Rebounds', player_one: 8.6, player_two: RegularStats["Stephen Curry"].REB, cap: 30},
+                {category: 'Assists', player_one:8.7, player_two: RegularStats["Stephen Curry"].AST, cap: 30},
+                {category: 'Free Throws', player_one: 4.8, player_two: RegularStats["Stephen Curry"].FTM, cap: 30},
+                {category: 'Turnovers', player_one: 4.1, player_two: RegularStats["Stephen Curry"].TOV, cap: 30},
+                {category: 'Field Goals', player_one: 9.9, player_two: RegularStats["Stephen Curry"].FGM, cap: 30},
+            ],
+            playoffData: [
+                {category: 'Steals', player_one: 1.2, player_two: PlayoffStats["Stephen Curry"].STL, cap: 30},
+                {category: 'Rebounds', player_one: 8.6, player_two: PlayoffStats["Stephen Curry"].REB, cap: 30},
+                {category: 'Assists', player_one:8.7, player_two: PlayoffStats["Stephen Curry"].AST, cap: 30},
+                {category: 'Free Throws', player_one: 4.8, player_two: PlayoffStats["Stephen Curry"].FTM, cap: 30},
+                {category: 'Turnovers', player_one: 4.1, player_two: PlayoffStats["Stephen Curry"].TOV, cap: 30},
+                {category: 'Field Goals', player_one: 9.9, player_two: PlayoffStats["Stephen Curry"].FGM, cap: 30},
             ]
         }
     }
@@ -34,15 +42,17 @@ class  TwoLevelPieChart extends Component {
         switch (season) {
             case 'regular':
                 this.setState({ playerNames: RegularNames })
+                break;
             case 'playoffs':
                 this.setState({ playerNames: PlayoffNames })
+                break;
             default:
                 break;
         }
     }
 
     shouldComponentUpdate(nextProps, nextState) {
-        if(nextState.currentPlayer !== this.state.currentPlayer){
+        if((nextState.currentPlayer !== this.state.currentPlayer) || (nextState.season !== this.state.season)){
             return true;
         }else{
             return false;
@@ -50,37 +60,60 @@ class  TwoLevelPieChart extends Component {
     }
 
     plotPlayer = (e) => {
-        let { season, data} = this.state;
+        let { season, regularData, playoffData} = this.state;
         let name = e.target.value;
-        let playerStats = season === 'regular' ? RegularStats[name] : PlayoffStats[name]
+        let playerStats = season === 'regular' ? RegularStats[name] : PlayoffStats[name];
+        
+        switch (season) {
+            case 'regular':
+                regularData[0].player_two = playerStats.STL;
+                regularData[1].player_two = playerStats.REB;
+                regularData[2].player_two = playerStats.AST;
+                regularData[3].player_two = playerStats.FTM;
+                regularData[4].player_two = playerStats.TOV;
+                regularData[5].player_two = playerStats.FGM;
 
-        data[0].player_two = playerStats.STL;
-        data[1].player_two = playerStats.REB;
-        data[2].player_two = playerStats.AST;
-        data[3].player_two = playerStats.FTM;
-        data[4].player_two = playerStats.TOV;
-        data[5].player_two = playerStats.FGM;
+                this.setState({regularData: regularData, currentPlayer: name})
+                break;
+            case 'playoffs':
+                playoffData[0].player_two = playerStats.STL;
+                playoffData[1].player_two = playerStats.REB;
+                playoffData[2].player_two = playerStats.AST;
+                playoffData[3].player_two = playerStats.FTM;
+                playoffData[4].player_two = playerStats.TOV;
+                playoffData[5].player_two = playerStats.FGM;
 
-        this.setState({
-            currentPlayer: name,
-            data: data
-        })
+                this.setState({playoffData: playoffData, currentPlayer: name})
+                break;
+        
+            default:
+                break;
+        }
     }
 
     handleFilter = (e) => {
+        let { playerNames } = this.state;
         let filter = e.target.value;
-        this.setState({
-            season: filter
-        })
+        if(filter === 'playoffs') {
+            this.setState({
+                season: filter,
+                playerNames: PlayoffNames
+            })
+        } else {
+            this.setState({
+                season: filter,
+                playerNames: RegularNames
+            })
+        }
     }
 
     render () {
-        const { data, season, playoffsData, playerNames } = this.state;
+        const { regularData, season, playoffData, playerNames } = this.state;
         
         return (
             <div>
                 <ResponsiveContainer width='100%' height={300}>
-                    <RadarChart cx='50%' cy='50%' outerRadius='80%' data={ season === 'regular' ? data : playoffsData}>
+                    <RadarChart cx='50%' cy='50%' outerRadius='80%' data={ season === 'regular' ? regularData : playoffData}>
                         <PolarGrid />
                         <PolarAngleAxis dataKey="category" />
                         <PolarRadiusAxis angle={30} domain={[0, 15]} />
@@ -92,7 +125,7 @@ class  TwoLevelPieChart extends Component {
                 </ResponsiveContainer>
                 <div className="radar-options">
                     <FilterGameType handleChange={(e)=>this.handleFilter(e)} />
-                    <SelectPlayer handleChange={(e)=>this.plotPlayer(e)} playerNames={playerNames} />
+                    <SelectPlayer handleChange={(e)=>this.plotPlayer(e)} playerNames={playerNames} season={season} />
                 </div>
             </div>
         );
--- client/src/components/SelectPlayer.jsx
+++ client/src/components/SelectPlayer.jsx
@@ -4,20 +4,35 @@ const SelectPlayer = (props) => {
     
     
     const renderNames = () => {
-        const { playerNames } = props;
-        
-        return(
-            <div className="player-names">
-                <label htmlFor="payler-list">Player: </label>
-                <select className="player-list" name="player-name" onChange={(e)=>props.handleChange(e)}>
-                    {Object.values(playerNames).map((item, i)=>{
-                        return(
-                            <option value={item} key={i}>{item}</option>
-                        )
-                    })}
-                </select>
-            </div>
-        )
+        const { playerNames, season } = props;
+
+        if(season === 'playoff'){
+            return(
+                <div className="player-names">
+                    <label htmlFor="payler-list">Player: </label>
+                    <select className="player-list" name="player-name" onChange={(e)=>props.handleChange(e)}>
+                        {Object.values(playerNames).map((item, i)=>{
+                            return(
+                                <option value={item} key={i}>{item}</option>
+                            )
+                        })}
+                    </select>
+                </div>
+            )
+        }else{
+            return(
+                <div className="player-names">
+                    <label htmlFor="payler-list">Player: </label>
+                    <select className="player-list" name="player-name" onChange={(e)=>props.handleChange(e)}>
+                        {Object.values(playerNames).map((item, i)=>{
+                            return(
+                                <option value={item} key={i}>{item}</option>
+                            )
+                        })}
+                    </select>
+                </div>
+            )
+        }
     }
 
     return(
